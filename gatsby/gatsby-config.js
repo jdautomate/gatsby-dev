0f3fad700d2685e5b53effc8565669403c38b50d
Justin Leber <justin@openvpn.net>
2023-05-25 13:37:37 -0700

    first commit

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,56 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "WebDev Portfolio",
+    description: "This is WebDev Portfolio Site",
+    author: "@webdev",
+    twitterUsername: "@john_smilga",
+    image: "/twitter-img.png",
+    siteUrl: "http://localhost:8000",
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: process.env.STRAPI_APP_URL || `https://strapi-development.openvpn.in/api`,
+        accessToken: process.env.STRAPI_TOKEN,
+        queryLimit: 1000, // Default to 100
+        //   contentTypes : `jobs`, `projects`, `blogs`,
+        //   singleType : `about`
+        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
+        contentTypes: [`jobs`],
+        singleTypes: [],
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-webfonts`,
+    //   options: {
+    //     fonts: {
+    //       google: [
+    //         {
+    //           family: "Roboto",
+    //           variants: ["400", "700"],
+    //         },
+    //         { family: "Open Sans" },
+    //       ],
+    //     },
+    //   },
+    // },
+  ],
+}
