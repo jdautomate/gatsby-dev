62d1f02d463de30583311695259e54cda65d2c74
Justin Leber <justin@openvpn.net>
2023-05-30 14:35:56 -0700

    new starter

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,23 +1,23 @@
 /**
  * Configure your Gatsby site with this file.
  *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
 
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
   siteMetadata: {
-    title: "WebDev Portfolio",
-    description: "This is WebDev Portfolio Site",
+    title: "OpenVPN Web",
+    description: "openvpn.net website",
     author: "@webdev",
-    twitterUsername: "@john_smilga",
     image: "/twitter-img.png",
     siteUrl: "http://localhost:8000",
   },
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -28,29 +28,15 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.STRAPI_APP_URL || `https://strapi-development.openvpn.in/api`,
+        apiURL: process.env.STRAPI_API_URL || `https://strapi-development.openvpn.in`,
         accessToken: process.env.STRAPI_TOKEN,
         queryLimit: 1000, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
-        contentTypes: [`jobs`],
+        contentTypes: [],
         singleTypes: [],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-webfonts`,
-    //   options: {
-    //     fonts: {
-    //       google: [
-    //         {
-    //           family: "Roboto",
-    //           variants: ["400", "700"],
-    //         },
-    //         { family: "Open Sans" },
-    //       ],
-    //     },
-    //   },
-    // },
   ],
 }
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,17 +1,5 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/Layout"
-import Hero from "../components/Hero"
-import Services from "../components/Services"
-import Jobs from "../components/Jobs"
-import Projects from "../components/Projects"
-import Blogs from "../components/Blogs"
-export default () => {
-  return (
-    <Layout>
-      <Hero />
-      <Services />
-    </Layout>
-    )
+import * as React from "react"
+
+export default function Home() {
+  return <div>Hello world!</div>
 }
-// ...GatsbyImageSharpFluid
